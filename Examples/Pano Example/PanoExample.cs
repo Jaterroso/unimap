4d76612a3dc37e1681e7f871e5dbde8a7d2c1258
Vatsal Ambastha <adrenak_@outlook.com>
2020-04-14 01:01:35 +0530

    feat: Move to Task<> from Promise<>. UPM.
    Dependencies are now loaded as UPM packages
    UniMap is now exported as a UPM package
    APIs and inner workings of the wrapper are now based on C# Tasks
    
    BREAKING CHANGE: Promise support removed

---
+++ Examples/Pano Example/PanoExample.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+namespace Adrenak.UniMap.Examples {
+	public class PanoExample : MonoBehaviour {
+		public PanoRenderer view;
+		public List<string> ids;
+		public float delay;
+		public PanoSize size;
+		public TextureFormat format = TextureFormat.RGB24;
+
+		IEnumerator Start() {
+            UniMapInitializer.Initialize();
+
+			while (true) {
+				foreach (var id in ids) {
+					view.downloader.DownloadAndForget(id, size, format);
+					yield return new WaitForSeconds(delay);
+				}
+			}
+		}
+	}
+}
